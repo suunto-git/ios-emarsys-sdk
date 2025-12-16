74d4a2312929ddb8bbdfbe90434137a1503593d3
bestwangwang <bencodevelop@gmail.com>
2025-12-16 14:18:50 +0800

    added logs and check logic

--- Sources/MobileEngage/IAM/UI/MEIAMViewController.m
+++ Sources/MobileEngage/IAM/UI/MEIAMViewController.m
@@ -6,36 +6,102 @@
 #import "MEJSBridge.h"
 
 @interface EmarsysLogger : NSObject
-+ (void)log:(NSString *) msg;
++ (void)log:(NSString *)msg;
++ (void)saveFile:(NSString *)msg;
 @end
 
 @implementation EmarsysLogger
 
-+ (void)log:(NSString *) msg {
-    NSLog(msg);
-    
-    NSString * cachesPath = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)[0];
-    NSURL * url = [NSURL fileURLWithPath:cachesPath];
-    NSString * filename = @"Emarsys_log";
-    NSURL *fileURL = [[url URLByAppendingPathComponent:filename] URLByAppendingPathExtension:@"log"];
-    NSFileManager *fm = [NSFileManager defaultManager];
-    
-    if (![fm fileExistsAtPath:fileURL.path]) {
-        [fm createFileAtPath:fileURL.path contents:[NSData new] attributes:nil];
-    }
-    
-    NSFileHandle *fileHandle = [NSFileHandle fileHandleForWritingAtPath:fileURL.path];
-    [fileHandle seekToEndOfFile];
-    
-    NSData * shi = [@"\n" dataUsingEncoding:NSUTF8StringEncoding];
-    [fileHandle writeData:shi];
-    
-    NSDate * date = [[NSDate alloc] init];
-    NSString * log = [NSString stringWithFormat:@"%@ %@", date, msg];
-    
-    NSData * data = [log dataUsingEncoding:NSUTF8StringEncoding];
-    [fileHandle writeData:data];
+#pragma mark - Helper Methods
+
++ (NSURL *)emarsysLogDirectoryURL {
+    static NSURL *logDirURL;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSString *cachesPath = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).firstObject;
+        NSURL *cachesURL = [NSURL fileURLWithPath:cachesPath];
+        logDirURL = [cachesURL URLByAppendingPathComponent:@"emarsys_log" isDirectory:YES];
+        
+        // Ensure the directory exists
+        NSError *error;
+        if (![[NSFileManager defaultManager] fileExistsAtPath:logDirURL.path]) {
+            [[NSFileManager defaultManager] createDirectoryAtURL:logDirURL
+                                      withIntermediateDirectories:YES
+                                                       attributes:nil
+                                                            error:&error];
+            if (error) {
+                NSLog(@"[EmarsysLogger] Failed to create log directory: %@", error.localizedDescription);
+            }
+        }
+    });
+    return logDirURL;
 }
+
++ (NSString *)ISO8601StringFromDate:(NSDate *)date {
+    static NSDateFormatter *formatter;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        formatter = [[NSDateFormatter alloc] init];
+        formatter.locale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
+        formatter.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:0];
+        formatter.dateFormat = @"yyyy-MM-dd'T'HH-mm-ss.SSS'Z'"; // Safe for filenames
+    });
+    return [formatter stringFromDate:date];
+}
+
+#pragma mark - Public API
+
++ (void)log:(NSString *)msg {
+    if (!msg) return;
+    
+    // Console log
+    NSLog(@"EmarsysSDK: %@", msg);
+    
+    // Prepare log file URL
+    NSURL *logDir = [self emarsysLogDirectoryURL];
+    NSURL *logFileURL = [logDir URLByAppendingPathComponent:@"Emarsys_log.log"];
+    
+    // Format log entry
+    NSDate *now = [NSDate date];
+    NSString *timestampedLog = [NSString stringWithFormat:@"%@ %@", [self ISO8601StringFromDate:now], msg];
+    NSData *data = [[timestampedLog stringByAppendingString:@"\n"] dataUsingEncoding:NSUTF8StringEncoding];
+    
+    // Write safely (thread-safe via @synchronized or serial queue; here using simple file lock via NSFileCoordinator optional)
+    @synchronized(self) {
+        NSFileHandle *fileHandle = [NSFileHandle fileHandleForWritingToURL:logFileURL error:nil];
+        if (!fileHandle) {
+            // File doesn't exist; create it
+            [[NSFileManager defaultManager] createFileAtPath:logFileURL.path contents:nil attributes:nil];
+            fileHandle = [NSFileHandle fileHandleForWritingToURL:logFileURL error:nil];
+        }
+        if (fileHandle) {
+            [fileHandle seekToEndOfFile];
+            [fileHandle writeData:data];
+            [fileHandle closeFile]; // Important!
+        } else {
+            NSLog(@"[EmarsysLogger] Failed to open log file for writing: %@", logFileURL.path);
+        }
+    }
+}
+
++ (void)saveFile:(NSString *)msg {
+    if (!msg) return;
+    
+    NSURL *logDir = [self emarsysLogDirectoryURL];
+    NSDate *now = [NSDate date];
+    NSString *filename = [NSString stringWithFormat:@"Emarsys.%@", [self ISO8601StringFromDate:now]];
+    NSURL *logFileURL = [[logDir URLByAppendingPathComponent:filename] URLByAppendingPathExtension:@"log"];
+    
+    NSData *data = [msg dataUsingEncoding:NSUTF8StringEncoding];
+    if (!data) return;
+    
+    // Simply write the entire message as a new file
+    NSError *error;
+    if (![[NSFileManager defaultManager] createFileAtPath:logFileURL.path contents:data attributes:nil]) {
+        NSLog(@"[EmarsysLogger] Failed to save log file: %@, error: %@", logFileURL.path, error.localizedDescription);
+    }
+}
+
 @end
 
 @interface MEIAMViewController () <WKNavigationDelegate>
@@ -50,6 +116,15 @@
 
 #pragma mark - ViewController
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear: animated];
+    [EmarsysLogger log:@"viewDidAppear"];
+    
+    if (!self.webView.isLoading) {
+        [self checkWebpageContent];
+    }
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.modalPresentationStyle = UIModalPresentationOverFullScreen;
@@ -58,6 +133,8 @@
     [self.bridge setJsResultBlock:^(NSDictionary<NSString *, NSObject *> *result) {
         [weakSelf respondToJS:result];
     }];
+    
+    [EmarsysLogger log:@"viewDidLoad"];
 }
 
 - (void)viewDidDisappear:(BOOL)animated {
@@ -69,6 +146,8 @@
     [self.webView removeFromSuperview];
     self.webView = nil;
     [super viewDidDisappear:animated];
+    
+    [EmarsysLogger log:@"viewDidDisappear"];
 }
 
 #pragma mark - Public methods
@@ -92,7 +171,14 @@
         }
         [weakSelf.webView loadHTMLString:message
                                  baseURL:nil];
+        [EmarsysLogger log:@"loadMessage"];
+        [EmarsysLogger saveFile: message];
     });
+}
+
+- (void)dealloc
+{
+    [EmarsysLogger log:@"dealloc"];
 }
 
 #pragma mark - WKNavigationDelegate
@@ -103,11 +189,18 @@ didFinishNavigation:(null_unspecified WKNavigation *)navigation {
         __weak typeof(self) weakSelf = self;
         dispatch_async(dispatch_get_main_queue(), ^{
             weakSelf.completionHandler();
+            weakSelf.completionHandler = nil;
         });
     }
+    [EmarsysLogger log:@"didFinishNavigation"];
+}
+
+#pragma mark - Private methods
+
+- (void)checkWebpageContent {
     NSString *jsCheckEmpty = @"(function() { var body = document.body; if (!body) return true; var temp = body.cloneNode(true); var removeTags = temp.querySelectorAll('script, style, noscript, iframe'); for (var i = 0; i < removeTags.length; i++) { removeTags[i].remove(); } var text = temp.innerText || temp.textContent || ''; return text.trim() === ''; })();";
     __weak MEIAMViewController * weakSelf = self;
-    [webView evaluateJavaScript:jsCheckEmpty completionHandler:^(id _Nullable result, NSError * _Nullable error) {
+    [self.webView evaluateJavaScript:jsCheckEmpty completionHandler:^(id _Nullable result, NSError * _Nullable error) {
         if (error) {
             [EmarsysLogger log:[NSString stringWithFormat:@"execute JavaScript error: %@", error.localizedDescription]];
             return;
@@ -127,12 +220,11 @@ didFinishNavigation:(null_unspecified WKNavigation *)navigation {
     }];
 }
 
-#pragma mark - Private methods
-
 - (void)handleWebViewLoadError:(NSError *)error {
+    __weak MEIAMViewController * weakSelf = self;
     dispatch_async(dispatch_get_main_queue(), ^{
-        if ([self.loadErrorDelegate respondsToSelector:@selector(closeInAppWithCompletionHandler:)]) {
-            [self.loadErrorDelegate closeInAppWithCompletionHandler:nil];
+        if ([weakSelf.loadErrorDelegate respondsToSelector:@selector(closeInAppWithCompletionHandler:)]) {
+            [weakSelf.loadErrorDelegate closeInAppWithCompletionHandler:nil];
         }
     });
 }
